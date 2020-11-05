023fe5b4a0360e295e808ef9e3b7a7801508b51a
Ivan <vaaeking@gmail.com>
2020-11-05 23:42:02 +0200

    done algo tasks 375 585 635

---
+++ algo-tasks/js/task1.js
@@ -0,0 +1,14 @@
+// ****** Task 375 ******
+
+function transformMatrix(initMatrix) {
+  let absMax = null;
+
+  initMatrix.forEach((row) => {
+    const rowMax = Math.max(...row.map((item) => Math.abs(item)));
+    if (rowMax > absMax || absMax === null) {
+      absMax = rowMax
+    }
+  })
+
+  return initMatrix.map((row) => row.map((item) => absMax ? item / absMax : 0));
+}
